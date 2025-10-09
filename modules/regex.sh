1f27aaeb8f2f5117193a30cc027bc6e78705ec73
dvsys <dvsys@localhost.localdomain>
2025-10-09 18:34:28 +0100

    [UPDATE] change the help message theme

--- modules/regex.sh
+++ modules/regex.sh
@@ -1,52 +1,51 @@
 #!/bin/bash
 # Show help if no args or --help used
 #
-#
-#
+# color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+RESET='\033[0m'
+
 #
 #
 #
 API_REGEX="$(dirname "${BASH_SOURCE[0]}")/../regex/api_regex.json"
 
-
-
 if [[ $# -eq 0 || "$1" == "--help" ]]; then
-  echo "  Usage: ./quevy regex [--module] [-f <file> / -d <directory> /  . <current directory]"
-  echo ""
-  echo "  --api     scan for existed regex such as google_api, firebase, google_captcha, google_oauth
-                    amazon_aws_access_key_id, amazon_mws_auth_toke, amazon_aws_url amazon_aws_url2
-                    amazon_aws_url2, facebook_access_token, authorization_basic, authorization_bearer
-                    authorization_api, mailgun_api_key, twilio_account_id, twilio_api_key,
-                    twilio_app_sid, paypal_braintree_access_token, square_auth_secret, square_access_token
-                    stripe_restricted_api, github_access_token, rsa_private_key, ssh_dsa_private_key,
-                    ssh_dc_private_key, pgp_private_block, json_web_token, slack_token, SSH_privKey,
-                    Heroku_api_key, possible_creds
-  " 
-  echo "  --vuln    filter url by vuln pattren using url query param"
+  echo -e "${YELLOW}  Usage: ./quevy regex [--module] <target> ${RESET}"
+    
+
+  echo -e "${MAGENTA}  [options]${RESET}"
+  echo "  --api     check for existed regex in file" 
+  echo "  --param   filter url by vuln pattren using url query param"
   echo "            lfi, sqli, xss, redirect, ssrf, ssti, takeover, idor, "
+
+  echo -e "${MAGENTA}  [target]${RESET}"
+  echo -e "  -f <file>      Search specific file"
+  echo -e "  -d <directory> Search directory recursively"  
+  echo -e "  -d .           Current directory"
+
   exit 0
 fi
+
+
+
 case "$1" in
-  --api)
-    shift
-    while [[ $# -gt 0 ]]; do
-      case "$1" in
-        -f)
-          file_path="$2"  # Get the next argument as file path
-          shift 2
-          ;;
-        -d)
-          directory_path="$2"
-          shift 2
-          ;;
-        *)
-          echo "Unknown flag : $1" >&2
-          exit 1
-          ;;
-      esac
-    done
-    ;;
+    --api)
+        shift
+        ;;
+    --param)
+        shift
+        ;;
 esac
+
 
 
 function scanRegex_file () {
@@ -61,12 +60,6 @@ function scanRegex_file () {
         fi
     done
 }
-
-if [[ -f "$file_path" ]]; then
-   scanRegex_file "$file_path"
-else
-    echo "cannot find any file"
-fi 
 
 
 
